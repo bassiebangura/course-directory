a660018a098b552ed221eae7a6dd076908565df1
bassiebangura <devbassiebangura@gmail.com>
2019-09-06 19:35:56 -0400

    Add Redirect to courses route.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Header from "./Header";
+import Teachers from "./Teachers";
+import Courses from "./Courses";
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <Router>
+    <div className="container">
+      <Header />
+      <Route path="/" exact component={Home} />
+      <Route path="/About" component={About} />
+      <Route path="/Teachers" component={Teachers} />
+      <Route path="/Courses" component={Courses} />
+    </div>
+  </Router>
 );
 
 export default App;
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import {Redirect, NavLink, Route} from 'react-router-dom'
+import CSS from './courses/CSS'
+import HTML from './courses/HTML'
+import JavaScript from './courses/JavaScript'
+import { blueBright } from 'ansi-colors';
+
 
 const Courses = () => (
-  <div className="main-content courses">
+
+<div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html' activeStyle={{backgroundColor: blueBright}}>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
     
     {/* Write routes here... */}
+    <Route exact path="/courses" render={() => <Redirect to="/courses/HTML"/>} />
+    <Route path="/courses/CSS" component={CSS}  />
+    <Route path="/courses/HTML" component={HTML}  />
+    <Route path="/courses/JavaScript" component={JavaScript}  />
   </div>
-);
+ 
+  );
+
 
 export default Courses;

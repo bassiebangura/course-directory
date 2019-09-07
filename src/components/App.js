d779646cb88931ecc5b2e0a3bb5b7d2bd9dc4b61
bassiebangura <devbassiebangura@gmail.com>
2019-09-06 21:38:46 -0400

    Working version of directory.

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Header from "./Header";
 import Teachers from "./Teachers";
 import Courses from "./Courses";
 import NotFound from "./NotFound";
+import Featured from './Featured'
 
 
 const App = () => (
@@ -14,9 +15,10 @@ const App = () => (
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/About" component={About} />
-        <Route path="/Teachers" component={Teachers} />
-        <Route path="/Courses" component={Courses} />
+        <Route path="/about" component={About} />
+        <Route exact path="/teachers" component={Teachers} />
+        <Route path="/courses" component={Courses} />
+        <Route path='/teachers/:topic/:name' component={Featured} />
         <Route component={NotFound} />
       </Switch>
     </div>

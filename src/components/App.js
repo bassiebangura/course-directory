07689f594a70ebaf6312a32fdc17e9f2760c28d9
bassiebangura <devbassiebangura@gmail.com>
2019-09-06 20:12:20 -0400

    Add pageNotFound rendering.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Header from "./Header";
 import Teachers from "./Teachers";
 import Courses from "./Courses";
+import NotFound from "./NotFound";
 
 const App = () => (
   <Router>
     <div className="container">
       <Header />
-      <Route path="/" exact component={Home} />
-      <Route path="/About" component={About} />
-      <Route path="/Teachers" component={Teachers} />
-      <Route path="/Courses" component={Courses} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Teachers" component={Teachers} />
+        <Route path="/Courses" component={Courses} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </Router>
 );
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import {Redirect, NavLink, Route} from 'react-router-dom'
+import {Redirect, NavLink, Route, Switch} from 'react-router-dom'
 import CSS from './courses/CSS'
 import HTML from './courses/HTML'
 import JavaScript from './courses/JavaScript'
 import { blueBright } from 'ansi-colors';
+import NotFound from "./NotFound"
 
 
-const Courses = () => (
+const Courses = ({match}) => (
 
 <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><NavLink to='/courses/html' activeStyle={{backgroundColor: blueBright}}>HTML</NavLink></li>
-        <li><NavLink to='/courses/css'>CSS</NavLink></li>
-        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
+        <li><NavLink to={`${match.url}/html`} activeStyle={{backgroundColor: blueBright}}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
     
     {/* Write routes here... */}
-    <Route exact path="/courses" render={() => <Redirect to="/courses/HTML"/>} />
-    <Route path="/courses/CSS" component={CSS}  />
-    <Route path="/courses/HTML" component={HTML}  />
-    <Route path="/courses/JavaScript" component={JavaScript}  />
+    <Switch>
+    <Route exact path={`${match.path}`} render={() => <Redirect to={`${match.path}/html`}/>} />
+    <Route path={`${match.path}/html`} component={CSS}  />
+    <Route path={`${match.path}/css`} component={HTML}  />
+    <Route path={`${match.path}/javascript`} component={JavaScript}  />
+    <Route component={NotFound} />
+    </Switch>
+  
   </div>
  
   );
